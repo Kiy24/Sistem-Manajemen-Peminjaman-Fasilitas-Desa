fdcc67d6d0fc5647182f416cb067661a82f7a40c
Bagas Putra Anugerah <bagasputra28@student.upi.edu>
2025-12-18 17:08:10 +0700

    inventaris.c

--- inventaris.c
+++ inventaris.c
@@ -12,20 +12,41 @@ int totalBarang = 0;
 struct fasilitasDesa inventaris[MAX_FASILITAS];
 
 
-void TambahBarang() {
+void tambahBarang() {
     printf("Nama Barang : ");
     scanf("%s", inventaris[totalBarang].barang);
     printf("Jumlah Barang : ");
     scanf("%d",&inventaris[totalBarang].jumlahBarang);
+    printf("%s Berhasil ditambahkan\n", inventaris[totalBarang].barang);
 totalBarang++;
-    printf("%s Berhasil ditambahkan\n", inventaris[totalBarang-1].barang);
 }
-void ListBarang() {
+void listBarang() {
     if (totalBarang == 0){
         printf("Penyimpanan Masih Kosong!\n");}
         else {
             for (int i  = 0; i < totalBarang; i++) {
         printf("%d, %s, %d\n", i+1, inventaris[i].barang, inventaris[i].jumlahBarang);
         }
-     }   
+     }
     }
+
+void cariBarang() {
+    char cari[50];
+    int ketemu = 0;
+
+    printf("Masukkan nama barang yang ingin dicari: ");
+    scanf("%s", cari);
+
+    for (int i = 0; i < totalBarang; i++) {
+        if (strcmp(inventaris[i].barang, cari) == 0) {
+            printf("Barang ditemukan!\n");
+            printf("%d, %s, %d\n", i+1, inventaris[i].barang, inventaris[i].jumlahBarang);
+            ketemu = 1;
+            break;
+        }
+    }
+
+    if (!ketemu) {
+        printf("Barang %s tidak ditemukan dalam inventaris.\n", cari);
+    }
+}
