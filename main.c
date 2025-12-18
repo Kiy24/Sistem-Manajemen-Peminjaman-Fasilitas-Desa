230a33e47aa84ac638ab94e6a20b70f56d798e44
Vian540 <frafidhermansyah@gmail.com>
2025-12-18 17:27:26 +0700

    Add main function for borrowing items

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    char namaPeminjam[100];
+    char barangDipinjam[100];
+    int nominal;
+    printf("Masukkan Nama: ");
+    scanf("%s", &namaPeminjam);
+    printf("Ingin meminjam: ");
+    scanf("%s", &barangDipinjam);
+    printf("Nominal: ");
+    scanf("%d", &nominal);
+}
